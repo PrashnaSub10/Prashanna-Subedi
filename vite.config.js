2c7f8f76dad3823b2b03f4c821b68318ae79afcc
DewDrop <fordjtrial25@zohomail.com>
2026-06-16 17:05:44 +0545

    Fix Vite base path for custom root domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/react-portfolio-template/',
+    base: '/',
     plugins: [react()],
     server: {
         allowedHosts: ['www.prashanna-subedi.com.np', 'portfolio.prashanna-subedi.com.np', 'localhost', '127.0.0.1']
